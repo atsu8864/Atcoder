7ddaf990d4056f462dcac32bdfc5d9a6b9bf1ad0
atsu8864 <ac21305@gm.ibaraki-ct.ac.jp>
2021-06-24 21:47:02 +0900

    206_c 解説ac vectorの要素数が大きくなりすぎるとbad_allocが出るから気をつける

--- abc/206/c.cpp
+++ abc/206/c.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 using ll = long long;
 typedef pair<int,int> P;
 
@@ -7,10 +9,21 @@ const long double PI = (acos(-1));
 const int MOD = 1e9+7;
 const int vx[4] = {0,1,0,-1};
 const int vy[4] = {1,0,-1,0};
-using namespace std;
 
 int main() {
-
-
+    int n;
+    cin >> n;
+    map<int, int> m;
+    ll ans = 0;
+    for(int i = 0; i < n; ++i){
+        int b;
+        cin >> b;
+        if(i-m[b] > 0){
+            ans += i-m[b];
+        }
+        m[b]++;
+    }
+    cout << ans << endl;
     return 0;
+
 }
--- begin_contest.sh
+++ begin_contest.sh
@@ -26,6 +26,7 @@ template=$(cat <<EOS
 #include <bits/stdc++.h>
 using namespace std;
 
+#define rep(i, n) for(int i = 0; i < (int)(n); ++i)
 using ll = long long;
 typedef pair<int,int> P;
 
