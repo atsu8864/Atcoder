c73ec5dc4670e0a05ec43b48c3ef5a716dbc0a0b
atsu8864 <atsuya8864@i.softbank.jp>
2021-07-03 11:46:21 +0900

    abc 202_d パスカルの三角形とコンビネーション 大きい配列の場合はグローバルかstaticをつける 解説ac

--- abc/202/d.cpp
+++ abc/202/d.cpp
@@ -10,7 +10,32 @@ const int MOD = 1e9+7;
 
 
 int main() {
-
-
+    static ll c[61][61];
+    int a, b;
+    ll k;
+    cin >> a >> b >> k;
+    c[0][0] = 1;
+    rep(i, 60){
+        rep(j, i+1){
+            c[i+1][j] += c[i][j];
+            c[i+1][j+1] += c[i][j];
+        }
+    }
+    string ans;
+    while(a + b > 0){
+        ll x = 0;
+        if(a > 0){
+            x = c[a+b-1][a-1];
+        }
+        if(k <= x){
+            ans += 'a';
+            --a;
+        }else{
+            ans += 'b';
+            --b;
+            k -= x;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
