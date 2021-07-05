0656930b6646ae4e2a3e2f69cc80678c2ecd5924
atsu8864 <atsuya8864@i.softbank.jp>
2021-07-05 14:50:38 +0900

    abc 204_c BFSを全探索

--- abc/204/c.cpp
+++ abc/204/c.cpp
@@ -10,6 +10,34 @@ const int MOD = 1e9+7;
 
 
 int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> g[2010];
+    rep(i, m){
+        int a, b;
+        cin >> a >> b;
+        --a;--b;
+        g[a].push_back(b);
+    }
+    int ans = 0;
+    rep(i, n){
+        queue<int> que;
+        set<int> vis;
 
+        que.push(i);
+        vis.insert(i);
+        while(!que.empty()){
+            int cu = que.front();
+            que.pop();
+            for(int j : g[cu]){
+                if(!vis.count(j)){
+                    que.push(j);
+                    vis.insert(j);
+                }
+            }
+        }
+        ans += vis.size();
+    }
+    cout << ans << endl;
     return 0;
 }
